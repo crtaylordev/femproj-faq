8209d7abcc9d9e93574cb3886508c7ef0c1c867e
CR Taylor <157721366+crtaylordev@users.noreply.github.com>
2024-05-09 09:55:00 -0400

    Restructure HTML and rewrite JS for accessibility

--- script-main.js
+++ script-main.js
@@ -1,14 +1,19 @@
-const accordian = document.getElementsByClassName("accordian-open");
-let i;
-
-for (i = 0; i < accordian.length; i++) {
-    accordian[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        let content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
+window.addEventListener("DOMContentLoaded", (event) => {
+  let buttons = document.querySelectorAll("#accordion button");
+  buttons.forEach((button) => {
+    let content = button.nextElementSibling;
+    button.addEventListener("click", (event) => {
+      if (button.classList.contains("active")) {
+        button.classList.remove("active");
+        button.setAttribute("aria-expanded", false);
+        content.style.maxHeight = null;
+        content.setAttribute("aria-hidden", true);
+      } else {
+        button.classList.add("active");
+        button.setAttribute("aria-expanded", true);
+        content.style.maxHeight = content.scrollHeight + "px";
+        content.setAttribute("aria-hidden", false);
+      }
     });
-}
+  });
+});
